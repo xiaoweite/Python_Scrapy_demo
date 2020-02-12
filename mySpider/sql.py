f1a627fe57e3188299beceb2a638515fcbb1d64c
e <1096329747@qq.com>
2020-02-12 16:12:08 +0800

    sourcetree测试提交代码

--- mySpider/sql.py
+++ mySpider/sql.py
@@ -7,7 +7,7 @@ import pytz
 from . import settings
 
 db = pymysql.connect(settings.MYSQL_HOST, settings.MYSQL_USER, settings.MYSQL_PASSWORD, settings.MYSQL_DB,
-                     settings.MYSQL_PORT, charset=settings.MYSQL_CHARSET)
+                     settings.MYSQL_PORT, charset=settings.MYSQL_CHARSET,cursorclass=pymysql.cursors.DictCursor)
 cursor = db.cursor()
 
 class TeacherSql(object):
@@ -16,16 +16,21 @@ class TeacherSql(object):
         sql = "INSERT INTO `teacher`" \
               "(`name`, `title`, `info`)" \
               "VALUES ('%s', %s, %s)" % \
-              (item['name'], db.escape(item['title']), db.escape(item['info']))
+              (item['name'], item['title'], db.escape(item['info']))
         try:
             cursor.execute(sql)
             db.commit()
             print('add_teacher--[name]:', item['name'])
         except pymysql.MySQLError as e:
+            cls.fp = open('sql.log', 'w+', encoding='utf-8')
+            text = str(e) + "\n"
+            cls.fp.write(text)
+
             # with open('./log/sql.log', 'r+') as i:
             #     i.write('profile sql error![error]:' + str(e))
             print(e)
             cursor.rollback()
+            cls.fp.close()
         pass
 
 class ReviewSql(object):
