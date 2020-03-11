227662aba6b8f3f3af505a597ed09baecde1e134
Lanson <lanson.li@livelocity.cn>
2020-03-11 11:33:07 +0800

    更新快代理IP存入方法

--- mySpider/spiders/zhimadaili_spider.py
+++ mySpider/spiders/zhimadaili_spider.py
@@ -7,16 +7,19 @@ import redis
 # orderid = ''  # 暂无购买订单
 
 # 提取代理链接，以私密代理为例
-# api_url = "https://dps.kdlapi.com/api/getdps/?orderid={}&num=1&pt=1&dedup=1&format=json&sep=1"
+# api_url = "https://dps.kdlapi.com/api/getdps/?orderid=908278477739119&num=1&pt=1&dedup=1&format=json&sep=1"
 
 # 提取独享IP
+# 芝麻代理
 api_url = "http://http.tiqu.alicdns.com/getip3?num=1&type=2&pro=440000&city=440100&yys=0&port=1&pack=82738&ts=1&ys=1&cs=1&lb=1&sb=0&pb=45&mr=1&regions=&gm=4"
 # api_url = "http://http.tiqu.alicdns.com/getip3?num=1&type=2&pro=&city=0&yys=0&port=1&pack=82738&ts=0&ys=0&cs=1&lb=1&sb=0&pb=45&mr=1&regions=&gm=4"
+# 快代理
+k_api_url = "https://dps.kdlapi.com/api/getdps/?orderid=908278477739119&num=1&pt=1&dedup=1&format=json&sep=1"
 
 logger = logging.getLogger(__name__)
 
 
-def fetch_proxy():
+def zima_fetch_proxy():
     """
         提取代理
     """
@@ -35,13 +38,32 @@ def fetch_proxy():
 
     return ips
 
+def kuai_fetch_proxy():
+    """
+        提取代理
+    """
+    # fetch_url = api_url.format(orderid)
+    r = requests.get(k_api_url)
+    if r.status_code != 200:
+        logger.error("fail to fetch proxy")
+        return False
+    content = json.loads(r.content.decode('utf-8'))
+    if content['data']['proxy_list']:
+        ips = content['data']['proxy_list']
+    else:
+        logger.error("fail to fetch proxy: %s" % content['msg'])
+        ips = ''
+
+    return ips
+
 if __name__ == '__main__':
 
     r = redis.Redis(host='120.78.164.142', port='6379', db=0,password='bu8515859', decode_responses=True)
-    proxy_list = fetch_proxy()
+    proxy_list = kuai_fetch_proxy()
     proxy_ip_list = []
     for proxy in proxy_list:
-        ip_prot = '{}:{}'.format(proxy['ip'], proxy['port'])
+        # ip_prot = '{}:{}'.format(proxy['ip'], proxy['port'])
+        ip_prot = proxy
         proxy_ip_list.append(ip_prot)
     r.setex('proxy_ip_list', 600, ','.join(proxy_ip_list))
 
